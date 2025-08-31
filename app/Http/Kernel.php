c6c0c2b6afdad190700c4fdceddae9d8f285ac27
pedro coutinho <jose.coutinho@pdmfc.com>
2025-08-31 12:08:34 +0100

    fix: kernel block frontend routes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,13 +36,15 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\BlockInProduction::class,
         ],
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+        'frontend' => [
+            \App\Http\Middleware\BlockInProduction::class,
         ],
     ];
 
