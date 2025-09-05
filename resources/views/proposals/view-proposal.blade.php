78381dc80ea982cbf0cecf5ce6156cc57c584da5
pedro coutinho <jose.coutinho@pdmfc.com>
2025-09-05 09:50:14 +0100

    fix

--- resources/views/proposals/view-proposal.blade.php
+++ resources/views/proposals/view-proposal.blade.php
@@ -67,7 +67,7 @@
     <nav class="navbar navbar-expand-lg fixed-top" style="background-color: var(--primary-color);">
         <div class="container d-flex align-items-center justify-content-between">
             <a class="navbar-brand" href="#top">
-                <img src=" {{ asset('img/logo-transparente.png') }}" alt="Logo" class="navbar-logo" style="height:auto; width:120px;">
+                <img src=" {{ url($logotipo) }}" alt="Logo" class="navbar-logo" style="height:auto; width:120px;">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
