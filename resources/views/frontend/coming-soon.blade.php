bf69bdba12d797d2ef251702c4aa62811af7edca
pedro coutinho <jose.coutinho@pdmfc.com>
2025-09-12 11:19:19 +0100

    .

--- resources/views/frontend/coming-soon.blade.php
+++ resources/views/frontend/coming-soon.blade.php
@@ -2,6 +2,22 @@
 <html lang="en">
 
 <head>
+    <script>
+        (function(h, o, t, j, a, r) {
+            h.hj = h.hj || function() {
+                (h.hj.q = h.hj.q || []).push(arguments)
+            };
+            h._hjSettings = {
+                hjid: 6518381,
+                hjsv: 6
+            };
+            a = o.getElementsByTagName('head')[0];
+            r = o.createElement('script');
+            r.async = 1;
+            r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+    </script>
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-0NT5HLTZ2J"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
