08ea4e99ed8781adeed9bdecff1927984fa04e03
pedro coutinho <jose.coutinho@pdmfc.com>
2025-09-26 08:49:54 +0100

    fix

--- resources/views/frontend/partials/layout.blade.php
+++ resources/views/frontend/partials/layout.blade.php
@@ -18,7 +18,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-
+    <meta name="google-site-verification" content="8ey-yAqrOmo1lTV1ZnDmJdyRs8KDw-XoE128jpKtys4" />
     <!-- CSS FILES -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
